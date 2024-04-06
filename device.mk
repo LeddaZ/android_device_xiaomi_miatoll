494e80698115e7fc7bb0bfb4979e409eb8484060
Leonardo Ledda <leonardoledda@gmail.com>
2024-04-06 02:21:42 +0200

    miatoll: Force dark boot animation
    Signed-off-by: Leonardo Ledda <leonardoledda@gmail.com>

--- device.mk
+++ device.mk
@@ -378,10 +378,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
-# Platform
-PRODUCT_BOARD_PLATFORM := atoll
-PRODUCT_USES_QCOM_HARDWARE := true
-
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.xiaomi-libperfmgr

67f8cd52d4888fa187b13412f21d43b6ac6fd140
Leonardo Ledda <leonardoledda@gmail.com>
2024-04-06 15:04:47 +0200

    miatoll: Dexpreopt SystemUIGoogle
    Signed-off-by: Leonardo Ledda <leonardoledda@gmail.com>

--- device.mk
+++ device.mk
@@ -272,6 +272,7 @@ PRODUCT_PACKAGES += \
     libstagefrighthw
 
 # Optimizations
+PRODUCT_DEXPREOPT_SPEED_APPS += SystemUIGoogle
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt

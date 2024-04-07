763300c99aab21b3a4cf1429701f29f4edd3783e
Leonardo Ledda <leonardoledda@gmail.com>
2024-04-07 14:42:36 +0200

    miatoll: Drop optimizations
    Signed-off-by: Leonardo Ledda <leonardoledda@gmail.com>

--- device.mk
+++ device.mk
@@ -272,12 +272,6 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libstagefrighthw
 
-# Optimizations
-PRODUCT_DEXPREOPT_SPEED_APPS += SystemUIGoogle
-PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
-PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
-PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
-
 # Overlays
 PRODUCT_PACKAGES += \
     CurtanaGlobalPAFrameworksOverlay \
